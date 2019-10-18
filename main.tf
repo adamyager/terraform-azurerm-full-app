3ba421bb1065e37b9e9bdb5f5598cc6527f60b52
adamyager <52081264+adamyager@users.noreply.github.com>
2019-10-18 16:13:26 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,92 @@
+module "rg" {
+  source  = "app.terraform.io/jlinn/rg/azurerm"
+  version = "0.0.2"
+
+  location = "Central US"
+  name = "jlinn-demo"
+  
+}
+  
+module "virtual_network" {
+  source  = "app.terraform.io/jlinn/virtual-network/azurerm"
+  version = "0.0.5"
+
+  resource_group_location = module.rg.resource_group_location
+  resource_group_name = module.rg.resource_group_name
+  prefix = var.prefix
+}
+
+module "network_security_group" {
+  source  = "app.terraform.io/jlinn/network-security-group/azurerm"
+  version = "1.0.1"
+
+  location = "centralus"
+  resource_group_name = "${module.rg.resource_group_name}"
+  predefined_rules = [
+      {
+        name                   = "SSH"
+      },
+      {
+        name                   = "HTTP"
+      },
+      {
+        name                   = "HTTPS"
+      }
+  ]
+}
+  
+module "compute_module" {
+  source  = "app.terraform.io/jlinn/computeModule/azurerm"
+  version = "0.0.10"
+  resource_group_name = module.rg.resource_group_name
+  network_security_group_id = module.network_security_group.network_security_group_id
+  subnet_id = module.virtual_network.subnet_id
+  location = "centralus"
+}
+
+  resource "null_resource" "configure-cat-app" {
+  depends_on = [
+    "module.compute_module",
+  ]
+
+  # Terraform 0.11
+  # triggers {
+  #   build_number = "${timestamp()}"
+  # }
+
+  # Terraform 0.12
+  triggers = {
+    build_number = "${timestamp()}"
+  }
+
+  provisioner "file" {
+    source      = "files/"
+    destination = "/home/${var.admin_username}/"
+
+    connection {
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+      host     = module.compute_module.app_fqdn
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt -y update",
+      "sudo apt -y install apache2",
+      "sudo systemctl start apache2",
+      "sudo chown -R ${var.admin_username}:${var.admin_username} /var/www/html",
+      "chmod +x *.sh",
+      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
+    ]
+
+    connection {
+      type     = "ssh"
+      user     = "${var.admin_username}"
+      password = "${var.admin_password}"
+      host     = module.compute_module.app_fqdn
+    }
+  }
+}
+
---
+++ outputs.tf
@@ -0,0 +1,5 @@
+
+# Outputs file
+output "app_url" {
+  value = "${module.compute_module.app_url}"
+}
